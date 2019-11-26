ecb7881709dc12c454d8d5274f446ffd84a926bd
bytesloth <oneminecraft1@googlemail.com>
2019-11-26 10:08:17 +0100

    searchbar

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -14,6 +14,9 @@ const routes: Routes = [
     children: [
       { path: 'damage-details', loadChildren: '../damage-details/damage-details.module#DamageDetailsPageModule'},
       { path: 'object-manager-new', loadChildren: '../object-manager-new/object-manager-new.module#ObjectManagerNewPageModule' },
+      { path: 'object-manager-control-list', loadChildren: './object-manager-control-list/object-manager-control-list.module#ObjectManagerControlListPageModule' },
+      { path: 'object-manager-control-new', loadChildren: './object-manager-control-new/object-manager-control-new.module#ObjectManagerControlNewPageModule' },
+      { path: 'object-manager-control-view', loadChildren: './object-manager-control-view/object-manager-control-view.module#ObjectManagerControlViewPageModule' },
     ]
   },
   {
