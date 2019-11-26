1e9d8b74f8312a3cb52ea968645b96dcb9e6df33
lenne98 <40517866+lenne98@users.noreply.github.com>
2019-11-26 09:18:41 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'object-manager-new', loadChildren: '../object-manager-new/object-manager-new.module#ObjectManagerNewPageModule' },
+  { path: 'object-manager-new', loadChildren: './object-manager-new/object-manager-new.module#ObjectManagerNewPageModule' },
   { path: 'object-manager-control-list', loadChildren: './object-manager-control-list/object-manager-control-list.module#ObjectManagerControlListPageModule' },
   { path: 'object-manager-control-new', loadChildren: './object-manager-control-new/object-manager-control-new.module#ObjectManagerControlNewPageModule' },
   { path: 'object-manager-control-view', loadChildren: './object-manager-control-view/object-manager-control-view.module#ObjectManagerControlViewPageModule' },
