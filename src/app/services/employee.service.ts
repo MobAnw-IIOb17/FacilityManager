7b7e3eb2cfab0130e38c8504b7f8dc2f844d49b4
hibo98 <niklasmerkelt@mail.de>
2019-11-26 11:20:52 +0100

    Added employee service

---
+++ src/app/services/employee.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { Storage } from '@ionic/storage';
+
+import { Employee } from '../model/employee.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private employeeDb: any;
+
+  constructor() {
+    this.employeeDb = new Storage({
+      name: '__employeesDb',
+      storeName: '_employees',
+      driverOrder: ['sqlite', 'indexeddb', 'websql', 'localstorage']
+    });
+  }
+
+  getAllEmployees(): Promise<Employee[]> {
+    const employees: Employee[] = [];
+    return this.employeeDb.forEach((value: any, key: string) => {
+      employees.push(value);
+    }).then(() => {
+      return employees;
+    });
+  }
+
+  getEmployee(uid: string): Promise<Employee> {
+    return this.employeeDb.get(uid).then((e) => {
+      return e;
+    });
+  }
+}
