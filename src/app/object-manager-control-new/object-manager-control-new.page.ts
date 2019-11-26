0b6147881aec8aa33d7e301ea408700bb83286f3
lenne98 <40517866+lenne98@users.noreply.github.com>
2019-11-26 13:48:20 +0100

    Eigenschaften

--- src/app/object-manager-control-new/object-manager-control-new.page.ts
+++ src/app/object-manager-control-new/object-manager-control-new.page.ts
@@ -11,5 +11,10 @@ export class ObjectManagerControlNewPage implements OnInit {
 
   ngOnInit() {
   }
-
+  
+  addPropertie(){
+    console.log(document.getElementById("newControlProperties").innerHTML);
+    document.getElementById("newControlProperties").innerHTML += "<ion-item><ion-label position='floating'>Eigenschaft</ion-label><ion-input></ion-input></ion-item>";
+    console.log(document.getElementById("newControlProperties").innerHTML);
+  }
 }
