36ddbf79c80cf977bb7f0e258b7eae0a7c93e4da
Maxim <max.mikhayloff@gmail.com>
2019-12-03 15:33:51 +0100

    damages-reports.page.ts changed 'ndOnInit' changed.

--- src/app/damage-reports/damage-reports.page.ts
+++ src/app/damage-reports/damage-reports.page.ts
@@ -2,42 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {DamageService} from '../services/damage.service';
 import {Damage} from '../model/damage.model';
 import {NavController} from '@ionic/angular';
-import {Observable, throwError} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
 import {HttpClient} from '@angular/common/http';
-import {DamageDetailsPage} from '../damage-details/damage-details.page';
 
 @Component({
-  selector: 'app-damage-reports',
-  templateUrl: './damage-reports.page.html',
-  styleUrls: ['./damage-reports.page.scss'],
-  providers:  [ DamageService ]
+    selector: 'app-damage-reports',
+    templateUrl: './damage-reports.page.html',
+    styleUrls: ['./damage-reports.page.scss'],
+    providers: [DamageService]
 })
 export class DamageReportsPage implements OnInit {
-  damages: Damage[] = [];
+    damages: Damage[] = [];
 
-  constructor(public damageService: DamageService, public nav: NavController, protected httpClient: HttpClient) { }
+    constructor(public damageService: DamageService, public nav: NavController, protected httpClient: HttpClient) {
+    }
 
-ngOnInit() {
-  //   const subscription = this.damageService.getDamages().subscribe(damages => {
-  //     this.damages = damages;
-  //     subscription.unsubscribe();
-  //   });
-}
+    async ngOnInit() {
+        this.damages = await this.damageService.getAllDamages().then(res => this.damages = res);
+    }
 
-  openDamageById() {
-    this.nav.navigateForward('/tabs/damage-details');
-  }
-
-  // openDamageById1(id: string) {
-  //   this.nav.navigateForward('/tabs/damage-details', {id: id});
-  // }
-
-
-  // public getDamagesById(id: string): Observable<Damage> {
-  //   return this.httpClient.get<Damage>(`../assets/damage-list.json/${id}`).pipe(
-  //       map(data => new Damage().deserialize(data)),
-  //       catchError(() => throwError('User not found'))
-  //   );
-  // }
+    openDamageById() {
+        this.nav.navigateForward('/tabs/damage-details');
+    }
 }
