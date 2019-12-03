2bace61fca4c62db8d631d0009851985f9e92601
Uta <uta@terminal.com>
2019-12-03 11:31:11 +0100

    add network connection listener, to be tested!

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -6,11 +6,26 @@ import { Network } from '@ionic-native/network/ngx';
   providedIn: 'root'
 })
 export class NetworkService {
+  connectSubscription;
 
   constructor(private network: Network) { }
 
-  getNetworkType(): string {
-    return this.network.type;
+  startConnectWatch() {
+    this.connectSubscription = this.network.onConnect().subscribe(() => {
+      console.log('network connected!');
+      // We just got a connection but we need to wait briefly
+      // before we determine the connection type. Might need to wait.
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (this.network.type === 'wifi') {
+          console.log('we got a wifi connection, woohoo!');
+        }
+      }, 3000);
+    });
+  }
+
+  stopConnectWatch() {
+    this.connectSubscription.unsubscribe();
   }
 
 }
