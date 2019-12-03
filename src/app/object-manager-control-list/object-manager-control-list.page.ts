ed3a26077c65608da3e574e783b6cccd178fc26b
maxx87 <semmeldreck@gmail.com>
2019-12-03 11:36:35 +0100

    add example

--- src/app/object-manager-control-list/object-manager-control-list.page.ts
+++ src/app/object-manager-control-list/object-manager-control-list.page.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../services/employee.service';
-
-
+//import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-object-manager-control-list',
   templateUrl: './object-manager-control-list.page.html',
   styleUrls: ['./object-manager-control-list.page.scss'],
 })
-export class ObjectManagerControlListPage implements OnInit {
 
+export class ObjectManagerControlListPage implements OnInit {
   
-  
+  constructor() {  }
+
+  /*
   constructor(private employeeService: EmployeeService) {
     this.employeeService.getAllEmployees().then((list) => {
       list[0].uid;
     })
   }
-
+*/
   ngOnInit() {
   }
 
