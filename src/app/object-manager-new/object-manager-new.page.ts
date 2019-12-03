79072d6b885ad18d9b5cce40070c1a81ef3e9d1b
bytesloth <oneminecraft1@googlemail.com>
2019-12-03 10:28:44 +0100

    choose from predictiveSearch

--- src/app/object-manager-new/object-manager-new.page.ts
+++ src/app/object-manager-new/object-manager-new.page.ts
@@ -9,27 +9,44 @@ import { PropertyService } from '../services/property.service';
 export class ObjectManagerNewPage implements OnInit {
 
   cities = [];
+  city: string = 'Görlitz';
 
   constructor(private propertyService: PropertyService) { 
-    this.showItems();
+    this.hideItems();
   }
 
   ngOnInit() {
   }
 
   showItems() {
-    this.cities = ['Eins','Zwei','Drei'];
+    this.cities = ['Eins','Zwei','Drei','Drei2'];
+    //this.cities = this.propertyService.getPropertiesByCity()
   }
 
-  predictiveSearch(event) {
+  hideItems() {
+    this.cities = [];
+  }
+
+  chooseCity(chosenCity) {
+    this.city = chosenCity;
+    console.log(this.city);
+    document.getElementById('#city_searchbar').setAttribute('value', this.city);
+    this.cities = [];
+  }
+
+/**
+ * Sortiert die entsprechende Liste nach der Eingabe aus dem Event
+ * @param event wird benötigt,um den Inhalt der Searchbar zu ermitteln
+ * @param list
+ */
+  predictiveSearch(event, list : Array<String>) {
     this.showItems();
+    console.log(list);
     var val = event.target.value;
     this.cities = this.cities.filter((i) => {
       return (i.toLowerCase().indexOf(val.toLowerCase()) > -1);
     });
   }
 
-  hideItems() {
-    this.cities = [];
-  }
+
 }
