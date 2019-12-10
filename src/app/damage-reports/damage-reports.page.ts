a6f1f96cd6c66c94fb41730b5d90bd1b5d2ecafe
Maxim <max.mikhayloff@gmail.com>
2019-12-10 08:47:29 +0100

    page-reports.ts changed.

--- src/app/damage-reports/damage-reports.page.ts
+++ src/app/damage-reports/damage-reports.page.ts
@@ -1,42 +1,28 @@
 import {Component, OnInit} from '@angular/core';
 import {DamageService} from '../services/damage.service';
 import {Damage} from '../model/damage.model';
-import {Observable, throwError} from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+import {NavController} from '@ionic/angular';
 import {DamageDetailsPage} from '../damage-details/damage-details.page';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-damage-reports',
   templateUrl: './damage-reports.page.html',
   styleUrls: ['./damage-reports.page.scss'],
-  providers:  [ DamageService ]
+  providers: [DamageService]
 })
 export class DamageReportsPage implements OnInit {
   damages: Damage[] = [];
 
-  constructor(public damageService: DamageService, private router: Router, protected httpClient: HttpClient) { }
-
-ngOnInit() {
-  //   const subscription = this.damageService.getDamages().subscribe(damages => {
-  //     this.damages = damages;
-  //     subscription.unsubscribe();
-  //   });
-}
-
-  openDmgFromInTab() {
-    this.router.navigateByUrl('/tabs/damage-details');
+  constructor(public damageService: DamageService, private nav: NavController) {
   }
-  // openDamageById1(id: string) {
-  //   this.nav.navigateForward('/tabs/damage-details', {id: id});
-  // }
 
+  async ngOnInit() {
+    this.damages = await this.damageService.getAllDamages() as Damage[];
+  }
 
-  // public getDamagesById(id: string): Observable<Damage> {
-  //   return this.httpClient.get<Damage>(`../assets/damage-list.json/${id}`).pipe(
-  //       map(data => new Damage().deserialize(data)),
-  //       catchError(() => throwError('User not found'))
-  //   );
-  // }
+  openDamage(uid: string) {
+    this.nav.push(DamageDetailsPage, {
+      uid
+    });
+  }
 }
