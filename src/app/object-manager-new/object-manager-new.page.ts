a0c1fecb8220fb67043746f76e71c9d8b1152a54
bytesloth <oneminecraft1@googlemail.com>
2019-12-17 10:25:57 +0100

    object = new Property()

--- src/app/object-manager-new/object-manager-new.page.ts
+++ src/app/object-manager-new/object-manager-new.page.ts
@@ -3,6 +3,7 @@ import { PropertyService } from '../services/property.service';
 import { Router, NavigationExtras } from '@angular/router';
 import { FormGroup} from '@angular/forms';
 import { ToastController } from '@ionic/angular';
+import { Property } from '../model/property.model';
 
 @Component({
   selector: 'app-object-manager-new',
@@ -16,7 +17,7 @@ export class ObjectManagerNewPage implements OnInit {
   firmCities = [];
   objects = [];
   city: string = '';
-  object: string = '';  
+  object = new Property();  
   firmObjects = [];
 
   constructor(private toastController: ToastController, private propertyService: PropertyService, private router: Router) {
@@ -71,11 +72,10 @@ export class ObjectManagerNewPage implements OnInit {
 
   loadCities() {
       this.propertyService.getPropertyCities().then((items) => {
-        var cityDummy = ['Görlitz', 'Zittau', 'Großschönau'];
         if (items.length !== 0) {
           this.firmCities = items;
         } else {
-          this.firmCities = cityDummy;
+          this.firmCities = [];
         }
       });
   }
@@ -115,15 +115,15 @@ export class ObjectManagerNewPage implements OnInit {
         this.clearAList(this.cities);
       }
       show = true;
-      this.object = '';
+      this.object.street = '';
       this.clearAList(this.firmObjects);
       document.getElementById('#object_searchbar').setAttribute('value', '');
     } else {
       if (!firmList.includes(chosenString)) {
-        this.object = '';
+        this.object.street = '';
         this.clearAList(this.firmObjects);
       } else {
-        this.object = chosenString;
+        this.object.street = chosenString;
         this.clearAList(this.objects);
       }
     }
@@ -161,10 +161,9 @@ export class ObjectManagerNewPage implements OnInit {
    */
   openOMCListInTab() {
     if (this.firmCities.includes(this.city)) {
-      if (this.firmObjects.includes(this.object)) {
+      if (this.firmObjects.includes(this.object.street)) {
         let navigationExtras: NavigationExtras = {
           queryParams: {
-            city: this.city,
             object: this.object
           }
         };
