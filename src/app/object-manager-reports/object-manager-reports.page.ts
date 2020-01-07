1137051388e3361122a539dce0122431f7529132
maxx87 <semmeldreck@gmail.com>
2020-01-07 14:16:23 +0100

    add refresher

--- src/app/object-manager-reports/object-manager-reports.page.ts
+++ src/app/object-manager-reports/object-manager-reports.page.ts
@@ -19,7 +19,62 @@ export class ObjectManagerReportsPage implements OnInit {
       deleted: "1",
       hidden: "0",
       title: "",
-      street: "Obere Dorf",
+      street: "Obere Dorf 23",
+      zip: "02708",
+      city: "Löbau",
+      owner: "Hans"
+    });
+    
+    this.objectItems.push({
+      uid: "2",
+      deleted: "0",
+      hidden: "0",
+      title: "",
+      street: "Unter Dorf 2",
+      zip: "48302",
+      city: "Görlitz",
+      owner: "Peter"
+    });
+    
+    this.objectItems.push({
+      uid: "3",
+      deleted: "0",
+      hidden: "0",
+      title: "",
+      street: "Obere Schiller 6",
+      zip: "57493",
+      city: "Bautzen",
+      owner: "Achim"
+    });
+  }
+
+  navToControlManagerNewPage() {
+    this.router.navigateByUrl('/tabs/object-manager-new');
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    //Hier Daten Aktualieren
+    this.Data1();
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 800);
+  }
+
+  Data1() {
+    this.objectItems.pop();
+    this.objectItems.pop();
+    this.objectItems.pop();   
+    
+    this.objectItems.push({
+      uid: "1",
+      deleted: "1",
+      hidden: "0",
+      title: "",
+      street: "Obere Dorf ",
       zip: "02708",
       city: "Löbau",
       owner: "Hans"
@@ -38,7 +93,7 @@ export class ObjectManagerReportsPage implements OnInit {
     
     this.objectItems.push({
       uid: "3",
-      deleted: "0",
+      deleted: "1",
       hidden: "0",
       title: "",
       street: "Obere Schiller",
@@ -46,9 +101,5 @@ export class ObjectManagerReportsPage implements OnInit {
       city: "Bautzen",
       owner: "Achim"
     });
-  }
-
-  navToControlManagerNewPage() {
-    this.router.navigateByUrl('/tabs/object-manager-new');
   }
 }
