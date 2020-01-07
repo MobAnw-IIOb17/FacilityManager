afd4bce3e43b1ee6ac0ee0bc77ba20ca8141c558
Maxim <max.mikhayloff@gmail.com>
2020-01-07 13:45:01 +0100

    damage-reports.page changed.

--- src/app/damage-reports/damage-reports.page.ts
+++ src/app/damage-reports/damage-reports.page.ts
@@ -61,7 +61,8 @@ export class DamageReportsPage implements OnInit {
       property: undefinedProperty,
       employee: undefinedEmployee,
       description: 'undefinedDescription',
-      images: []
+      images: [],
+      location: 'undefinedLocation'
     }
 
     this.damages.push(this.damage);
