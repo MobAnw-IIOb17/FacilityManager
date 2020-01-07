91347550d76d92fcc493905d106b9298d26961f6
Julius Ploß <julius.ploss@web.de>
2020-01-07 09:47:02 +0100

    re-added AppCameraService

--- src/app/damage-form/damage-form.page.ts
+++ src/app/damage-form/damage-form.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppCameraService} from '../services/app-camera.service';
 
 @Component({
   selector: 'app-damage-form',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DamageFormPage implements OnInit {
 
-  constructor() { }
+  constructor(public appCameraService: AppCameraService) { }
 
   ngOnInit() {
   }
