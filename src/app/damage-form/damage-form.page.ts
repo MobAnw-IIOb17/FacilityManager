5c235a898f16c3c378ca5a1c5e4cc46cad6f4e75
Maximal800 <57709972+Maximal800@users.noreply.github.com>
2020-01-07 09:54:35 +0100

    Added Function for Damage-Form Gallery

--- src/app/damage-form/damage-form.page.ts
+++ src/app/damage-form/damage-form.page.ts
@@ -8,8 +8,29 @@ import {AppCameraService} from '../services/app-camera.service';
 })
 export class DamageFormPage implements OnInit {
 
-  constructor(public appCameraService: AppCameraService) { }
+  public pictures: String[] = [];
+
+  constructor(
+    public appCameraService: AppCameraService
+  ) { }
 
   ngOnInit() {
+  }
+
+  addPictureCamera() {
+    var ImageData = this.appCameraService.takePicture();
+    this.pictures.push('data:image/jpeg;base64,'+ImageData);
+    var gallery = document.getElementById('form_gallery');
+    if(! (Math.floor(this.pictures.length/3) == gallery.children.length)){
+      gallery.appendChild(document.createElement("ion-row"));
+    }
+    var row = gallery.children[gallery.children.length-1];
+    if(! ((this.pictures.length-(Math.floor(this.pictures.length/3)*3)) == row.children.length)){
+      row.appendChild(document.createElement("ion-col"));
+    }
+    var col = row.children[row.children.length-1];
+    var newPicture = document.createElement("ion-img");
+    newPicture.setAttribute("[src]", '"pictures['+ (this.pictures.length-1) +']" *ngIf="pictures['+ (this.pictures.length-1) +']"');
+    col.appendChild(document.createElement("ion-thumbnail").appendChild(newPicture));
   }
 }
