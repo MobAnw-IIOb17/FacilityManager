7a08edf76f08d279878b8c869682b9ca76213ba0
Maximal800 <57709972+Maximal800@users.noreply.github.com>
2020-01-07 19:13:27 +0100

    added deleteEvent in gallery.service

--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -6,7 +6,7 @@ import { AppCameraService } from '../services/app-camera.service';
 })
 export class GalleryService {
 
-  public imgBase64: string[];
+  public imgBase64: string[] = [];
   public galleryHTML;
 
   constructor(
@@ -14,9 +14,34 @@ export class GalleryService {
   ) { }
 
   addCameraPicture() {
-    var ImageData = this.appCameraService.takePicture();
+    //var ImageData = this.appCameraService.takePicture();
+    var ImageData = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==';
     this.imgBase64.push('data:image/png;base64,'+ImageData);
     this.addToGallery(this.imgBase64[(this.imgBase64.length-1)]);
+  }
+
+  addGalleryPicture() {
+    //TODO
+  }
+
+  openDialog = () => {
+
+  }
+
+  deleteFromGallery = (index: number) => {
+    var imageClipboard = this.imgBase64;
+    this.imgBase64 = [];
+    while (this.galleryHTML.hasChildNodes()) {  
+      this.galleryHTML.removeChild(this.galleryHTML.firstChild);
+    } 
+    var i = 0;
+    imageClipboard.forEach(element => {
+      if(i!=index){
+        this.imgBase64.push(element);
+        this.addToGallery(element); 
+      }
+      i++;
+    })
   }
 
   addToGallery(src: string) {
@@ -29,13 +54,13 @@ export class GalleryService {
       var col = row.children[row.children.length-1];
       var newPicture = document.createElement("ion-img");
       newPicture.setAttribute("src", src);
+      newPicture.setAttribute("id",(this.imgBase64.length-1)+"_Img");
+      newPicture.addEventListener("click", () => { this.deleteFromGallery((this.imgBase64.length-1)) });
       col.appendChild(newPicture);
     } else {
       this.galleryHTML = document.getElementById('gallery-grid');
       this.addToGallery(src); 
     }
   }
-
-
 
 }
