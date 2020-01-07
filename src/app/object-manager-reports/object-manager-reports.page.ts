de55b14b8abf2e7ffbdd7f40b8c134f928e66067
maxx87 <semmeldreck@gmail.com>
2020-01-07 10:50:05 +0100

    add content obj manager reports

--- src/app/object-manager-reports/object-manager-reports.page.ts
+++ src/app/object-manager-reports/object-manager-reports.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Property } from '../model/property.model';
 
 @Component({
   selector: 'app-object-manager-reports',
@@ -8,9 +9,43 @@ import { Router } from '@angular/router';
 })
 export class ObjectManagerReportsPage implements OnInit {
 
+  objectItems: Array<Property> = []
+
   constructor(private router: Router) { }
 
   ngOnInit() {
+    this.objectItems.push({
+      uid: "1",
+      deleted: "1",
+      hidden: "0",
+      title: "",
+      street: "Obere Dorf",
+      zip: "02708",
+      city: "Löbau",
+      owner: "Hans"
+    });
+    
+    this.objectItems.push({
+      uid: "2",
+      deleted: "1",
+      hidden: "0",
+      title: "",
+      street: "Unter Dorf",
+      zip: "48302",
+      city: "Görlitz",
+      owner: "Peter"
+    });
+    
+    this.objectItems.push({
+      uid: "3",
+      deleted: "0",
+      hidden: "0",
+      title: "",
+      street: "Obere Schiller",
+      zip: "57493",
+      city: "Bautzen",
+      owner: "Achim"
+    });
   }
 
   navToControlManagerNewPage() {
