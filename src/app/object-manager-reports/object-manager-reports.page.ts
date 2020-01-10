beffd8a004e08e3b4f2cb0c0540311a267f9f56a
maxx87 <semmeldreck@gmail.com>
2020-01-10 12:52:03 +0100

    ObjBrowser load Obj

--- src/app/object-manager-reports/object-manager-reports.page.ts
+++ src/app/object-manager-reports/object-manager-reports.page.ts
@@ -11,49 +11,19 @@ import { ObjectChecklist } from '../model/object-checklist.model';
   templateUrl: './object-manager-reports.page.html',
   styleUrls: ['./object-manager-reports.page.scss'],
 })
+
 export class ObjectManagerReportsPage implements OnInit {
 
-  objectItems: Array<Property> = []
-  dataReturned:any
-  objectChecklists: ObjectChecklist[] = [];
-  objectChecklist: ObjectChecklist;
+  private objectItems: Array<Property> = [];
+  private objectChecklists: ObjectChecklist[] = [];
+  private objectChecklist: ObjectChecklist = new ObjectChecklist();
 
-  constructor(private router: Router, private popoverController:PopoverController, private objectChecklistService:ObjectChecklistService) { }
+  constructor(
+    private router: Router, 
+    private popoverController:PopoverController, 
+    private objectChecklistService:ObjectChecklistService) { }
 
-  ngOnInit() {
-    this.objectItems.push({
-      uid: "1",
-      deleted: "1",
-      hidden: "0",
-      title: "",
-      street: "Obere Dorf 23",
-      zip: "02708",
-      city: "Löbau",
-      owner: "Hans"
-    });
-    
-    this.objectItems.push({
-      uid: "2",
-      deleted: "0",
-      hidden: "0",
-      title: "",
-      street: "Unter Dorf 2",
-      zip: "48302",
-      city: "Görlitz",
-      owner: "Peter"
-    });
-    
-    this.objectItems.push({
-      uid: "3",
-      deleted: "0",
-      hidden: "0",
-      title: "",
-      street: "Obere Schiller 6",
-      zip: "57493",
-      city: "Bautzen",
-      owner: "Achim"
-    });
-  }
+  ngOnInit() { }
 
   /**
    * Route zur Nächsten Seite um eine Kontrolle zu erstellen
@@ -63,18 +33,31 @@ export class ObjectManagerReportsPage implements OnInit {
   }
 
   /**
-   * Beim Zeihen nach unten werden alle Elemente aktualisert
+   * Aktualisiert das Checklist Objekt für die Anzeige
+   * vorher wird es gelöscht
+   */
+  refreshChecklistItems() {
+    setTimeout(() => {
+      this.objectChecklistService.getAllChecklists().then((items) => {
+        console.log("items.length: " + items.length);
+        if(items.length !== 0) {
+          console.log(items);
+          this.objectChecklists = [];
+          this.objectChecklists = items;
+        }
+      });
+    }, 800);
+  }
+
+  /**
+   * Beim Zeihen nach unten auf der Page, werden alle Elemente aktualisiert
    * 
-   * @param event Event 
+   * @param event Refresh Event 
    */
   doRefresh(event) {
-    console.log('Begin async operation');
-
-    //Hier Daten Aktualieren
-    this.Data1();
+    this.refreshChecklistItems();
 
     setTimeout(() => {
-      console.log('Async operation has ended');
       event.target.complete();
     }, 800);
   }
@@ -82,9 +65,9 @@ export class ObjectManagerReportsPage implements OnInit {
   /**
    * Öffnet ein PopOver oben Rechts mit einem Menu für die Auswahl
    *  - Aktualisieren
-   *  - Sortieren nach Stadt, Datum, Status
+   *  - Sortieren nach Stadt, Status
    * 
-   * unter onDidDismiss() wird die Rückgabe des popover verarbeitet
+   * unter onDidDismiss() wird die Rückgabe des Popover verarbeitet
    */
   async openPopover() {
     const popover = await this.popoverController.create({
@@ -94,71 +77,20 @@ export class ObjectManagerReportsPage implements OnInit {
 
     popover.onDidDismiss().then((dataReturned) => {
       if(dataReturned !== null) {
-        this.dataReturned = dataReturned.data;
         if(dataReturned.data === "refresh") {
-          //this.Data1();
-
-          this.objectChecklistService.getAllChecklists().then((items) => {
-            console.log("items.length: " + items.length);
-            if(items.length !== 0) {
-              console.log(items);
-            }
-          });
-
-        
-
+          this.refreshChecklistItems();
         }
         if(dataReturned.data === "city") {
-          this.objectItems.sort((a, b) => (a.city > b.city) ? 1 : -1)
+          this.objectChecklists.sort((a, b) => (a.property.city > b.property.city) ? 1 : -1)
         }
         if(dataReturned.data === "date") {
         }
         if(dataReturned.data === "status") {
-          this.objectItems.sort((a, b) => (a.deleted > b.deleted) ? 1 : -1)
+          this.objectChecklists.sort((a, b) => (a.sent > b.sent) ? 1 : -1)
         }
-        console.log("Return: " + this.dataReturned)
       }
     })
 
     return await popover.present();
-  }
-
-  Data1() {
-    this.objectItems.pop();
-    this.objectItems.pop();
-    this.objectItems.pop();   
-    
-    this.objectItems.push({
-      uid: "1",
-      deleted: "1",
-      hidden: "0",
-      title: "",
-      street: "Obere Dorf ",
-      zip: "02708",
-      city: "Löbau",
-      owner: "Hans"
-    });
-    
-    this.objectItems.push({
-      uid: "2",
-      deleted: "1",
-      hidden: "0",
-      title: "",
-      street: "Unter Dorf",
-      zip: "48302",
-      city: "Görlitz",
-      owner: "Peter"
-    });
-    
-    this.objectItems.push({
-      uid: "3",
-      deleted: "1",
-      hidden: "0",
-      title: "",
-      street: "Obere Schiller",
-      zip: "57493",
-      city: "Bautzen",
-      owner: "Achim"
-    });
   }
 }
