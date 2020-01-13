85f792785f67630d0eddd1acf21dd51c521ee01d
Julius Ploß <julius.ploss@web.de>
2020-01-13 22:58:23 +0100

    removed example damages from DamageReports

--- src/app/damage-reports/damage-reports.page.ts
+++ src/app/damage-reports/damage-reports.page.ts
@@ -24,10 +24,6 @@ export class DamageReportsPage {
 
   async refreshDamages() {
     this.damages = await this.damageService.getAllDamages();
-
-    if (this.damages.length == 0) {
-      this.makeUndefinedDamage();
-    }
   }
 
   openDamage(damage: Damage) {
@@ -37,77 +33,5 @@ export class DamageReportsPage {
       }
     };
     this.router.navigateByUrl('/tabs/damage-details', navigationExtras);
-  }
-
-
-  makeUndefinedDamage(){
-    let undefinedProperty: Property = {
-      uid: 'undefinedUid',
-      deleted: 'undefinedDeleted',
-      hidden: 'undefinedHidden',
-      title: 'undefinedTitle',
-      street: 'undefinedStreet',
-      zip: 'undefinedZip',
-      city: 'undefinedCity',
-      owner: 'undefinedMaier'
-    }
-
-    let undefinedEmployee: Employee = {
-      uid: 'undefinedUid',
-      name: 'undefinedName',
-      deleted: 'undefinedDeleted',
-      hidden: 'undefinedHidden'
-    }
-
-    this.damage = {
-      uid: 'undefinedUid',
-      createDate: 'undefinedCreateDate',
-      property: undefinedProperty,
-      employee: undefinedEmployee,
-      description: 'undefinedDescription',
-      images: [
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-      ],
-      location: 'undefinedLocation',
-      sent: false,
-      sentTimestamp: null,
-    }
-
-    this.damages.push(this.damage);
-
-    this.damage = {
-      uid: 'undefinedUid',
-      createDate: 'undefinedCreateDate',
-      property: undefinedProperty,
-      employee: undefinedEmployee,
-      description: 'undefinedDescription',
-      images: [
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-      ],
-      location: 'undefinedLocation',
-      sent: false,
-      sentTimestamp: null,
-    }
-
-    this.damages.push(this.damage);
-
-    this.damage = {
-      uid: 'undefinedUid',
-      createDate: 'undefinedCreateDate',
-      property: undefinedProperty,
-      employee: undefinedEmployee,
-      description: 'undefinedDescription',
-      images: [
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
-        'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
-      ],
-      location: 'undefinedLocation',
-      sent: false,
-      sentTimestamp: null,
-    }
-
-    this.damages.push(this.damage);
   }
 }
