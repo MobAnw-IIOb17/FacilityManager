83b0b9b2c00af7be11412377768019d913f84d9e
hibo98 <niklasmerkelt@mail.de>
2020-01-14 08:40:42 +0100

    Add artisans contact data to property model

--- src/app/model/property.model.ts
+++ src/app/model/property.model.ts
@@ -19,6 +19,8 @@
  * @var {string} owner
  *  the (real-life) owner of the property
  */
+import {Artisan} from './artisan.model';
+
 export class Property {
     uid: string;
     deleted: string;
@@ -28,4 +30,19 @@ export class Property {
     zip: string;
     city: string;
     owner: string;
+    // tslint:disable-next-line:variable-name
+    owner_street: string;
+    // tslint:disable-next-line:variable-name
+    owner_zip: string;
+    // tslint:disable-next-line:variable-name
+    owner_city: string;
+    administrator: Artisan;
+    elektriker: Artisan;
+    heizung: Artisan;
+    sanitear: Artisan;
+    schluesseldienst: Artisan;
+    schornsteinfeger: Artisan;
+    brandschutz: Artisan;
+    rohrreinigung: Artisan;
+    fahrstuhl: Artisan;
 }
